ae11274666742a42afd53c50c47189d3e9817bb5
FellipeDCastroo <fellipe.castro@sempreceub.com>
2026-03-14 16:32:36 -0300

    Commit inicial: Setup React, TypeScript e Tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">JF Code tá on! 🚀</h1>
+    </div>
+  )
+}
